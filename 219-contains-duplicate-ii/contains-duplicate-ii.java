4bccabe822baa582148e60799796c8a26a31f47b
vtu26535-sudo <vtu26535@veltech.edu.in>
2026-02-13 10:51:38 +0530

    Time: 22 ms (92.14%) | Memory: 90.8 MB (25.90%) - LeetSync

---
+++ 219-contains-duplicate-ii/contains-duplicate-ii.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer, Integer> lastSeen = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (lastSeen.containsKey(nums[i]) && i - lastSeen.get(nums[i]) <= k) {
+                return true;
+            }
+            lastSeen.put(nums[i], i);
+        }
+
+        return false;
+    }
+}
