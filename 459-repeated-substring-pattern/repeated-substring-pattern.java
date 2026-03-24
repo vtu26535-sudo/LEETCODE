0ea172666d8638bdb71e39c2184dc3c86182591e
vtu26535-sudo <vtu26535@veltech.edu.in>
2026-03-24 11:19:53 +0530

    Time: 74 ms (59.64%) | Memory: 46.6 MB (87.31%) - LeetSync

---
+++ 459-repeated-substring-pattern/repeated-substring-pattern.java
@@ -0,0 +1,7 @@
+class Solution {
+    public boolean repeatedSubstringPattern(String s) {
+        String doubled = s + s;
+        String trimmed = doubled.substring(1, doubled.length() - 1);
+        return trimmed.contains(s);
+    }
+}
